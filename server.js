3c43ceb04c3202dfcd37daac6ba0937770e1bbf3
Eduardo <eoalcantara@gmail.com>
2020-12-18 22:19:54 -0300

    Adicionando jwt fake

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const jsonServer = require('json-server');
+const auth = require('json-server-auth');
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+// /!\ Bind the router db to the app
+server.db = router.db
+
 const port = process.env.PORT || 8080;
 
 server.use(middlewares);
+server.use(auth)
 server.use(router);
 server.listen(port, ()=>{
     console.log(`JSON Server is running in ${port}`);
